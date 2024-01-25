c73f061b8d5785e130703b60ae028503de94b7f1
demiddima <demid.dima.1996@gmail.com>
2024-01-26 07:11:35 +1000

    add project 'guess the number'

--- CleannetCode/Program.cs
+++ CleannetCode/Program.cs
@@ -4,7 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            Console.WriteLine("Привет как тебя зовут?");
+            string? name = Console.ReadLine();
+            Console.WriteLine($"{name}, сейчас будем играть в 'Угадай число'. Я загадываю, ты отгадываешь");
+
+            Random rnd = new Random();
+            int guess = rnd.Next(0, 100);
+
+            Console.WriteLine("Так, ну всё, загадал число от 0 до 99. Угадывай, а я буду подсказывать тебе!");
+
+            bool isIntNumber = false;
+            int userNumber = -1;
+            bool isWin = false;
+
+            while(!isWin) 
+            {
+                do
+                {
+                    Console.WriteLine("Введи число: ");
+                    string? userInput = Console.ReadLine();
+                    isIntNumber = int.TryParse(userInput, out userNumber);
+
+                    if (!isIntNumber)
+                    {
+                        Console.WriteLine("Неверно введённое число. Введите число от 0 до 99");
+                    }
+
+                }
+                while (!isIntNumber);
+
+
+                if (userNumber == guess)
+                {
+                    Console.WriteLine($"Поздравляю {name}, ты угадал!");
+                    isWin = true;
+                }
+                else if (userNumber > guess)
+                {
+                    Console.WriteLine("Нет, загаданное число меньше)");
+                }
+                else
+                {
+                    Console.WriteLine("Нет, загаданное число больше)");
+                }
+            }
+
+           
+
+
+        }   
+
     }
 }
