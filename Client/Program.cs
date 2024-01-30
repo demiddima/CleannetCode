372ae285cd6c37545608487cffa549ca264d12fa
demiddima <demid.dima.1996@gmail.com>
2024-01-30 14:02:21 +1000

    add HttpClient

---
+++ Client/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var httpClient = new HttpClient();
+
+            var response = httpClient.GetAsync("https://github.com").Result;
+
+            var context = response.Content.ReadAsStringAsync().Result;
+            Console.WriteLine(context);
+        }
+    }
+}
